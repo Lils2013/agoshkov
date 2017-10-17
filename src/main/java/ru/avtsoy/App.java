6e785e93aeb46484f89214310015c8bea53fed89
Alexander Tsoy <Lils2010@gmail.com>
2017-10-17 17:13:39 +0300

    fixed

--- src/main/java/ru/avtsoy/App.java
+++ src/main/java/ru/avtsoy/App.java
@@ -1,23 +1,77 @@
 package ru.avtsoy;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
 public class App {
     private static final double TAU = 1;
     private static final double A = 1;
     private static final double B = 1;
-    private static final double H_X = 0.1;
-    private static final double H_Y = 0.1;
-    private static final double MU = 1/(2*Math.PI);
-    private static final double T = Math.PI/2;
+    private static final int N_X = 11;
+    private static final int N_Y = 11;
+    private static final double H_X = A / (N_X - 1);
+    private static final double H_Y = B / (N_Y - 1);
+    private static final double MU = 1 / (2 * Math.PI);
+    private static final double T = Math.PI / 2;
     private static final double ALPHA = 0.0001;
-    private static final int N_X = 10;
-    private static final int N_Y = 10;
-    private static double[][] phi  = new double[N_X][N_Y];
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    private static final int N_TIME_STEPS = 11;
+    private static final double TIME_STEP = T / (N_TIME_STEPS - 1);
+    private static final double[][][] f = new double[N_TIME_STEPS][N_X][N_Y];
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        double[][][] phi = new double[N_TIME_STEPS][N_X][N_Y];
+        phi = eqForPhi(phi);
+        double[][][] q = new double[N_TIME_STEPS][N_X][N_Y];
+        System.out.println(Arrays.deepToString(phi[N_TIME_STEPS-1]));
+    }
+
+    private static double[][][] eqForPhi(double[][][] phi) {
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                phi[0][j][k] = u0(j, k);
+            }
+        }
+        phi[0] = corner(phi[0]);
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                phi[1][j][k] = u1(j, k) * TIME_STEP + phi[0][j][k];
+            }
+        }
+        phi[1] = corner(phi[1]);
+        for (int i = 2; i < N_TIME_STEPS; i++) {
+            for (int j = 1; j < N_X - 1; j++) {
+                for (int k = 1; k < N_Y - 1; k++) {
+                    phi[i][j][k] = Math.pow(TIME_STEP, 2) * (f[i - 1][j][k] +
+                            MU * ((phi[i - 1][j - 1][k] - 2 * phi[i - 1][j][k] + phi[i - 1][j + 1][k]) / Math.pow(H_X, 2) +
+                                    (phi[i - 1][j][k - 1] - 2 * phi[i - 1][j][k] + phi[i - 1][j][k + 1]) / Math.pow(H_Y, 2))) +
+                            2 * phi[i - 1][j][k] - phi[i - 2][j][k];
+                }
+            }
+            phi[i] = corner(phi[i]);
+        }
+        return phi;
+    }
+
+    private static double u0(int j, int k) {
+        return 0;
+    }
+
+    private static double u1(int j, int k) {
+        return Math.sin(j * H_X) * Math.sin(k * H_Y);
+    }
+
+    private static double[][] corner(double[][] arr) {
+        for (int k = 0; k < N_Y; k++) {
+            arr[0][k] = 0;
+            arr[N_X - 1][k] = 0;
+        }
+        for (int j = 0; j < N_X; j++) {
+            arr[j][0] = 0;
+            arr[j][N_Y - 1] = 0;
+        }
+        return arr;
     }
 }
