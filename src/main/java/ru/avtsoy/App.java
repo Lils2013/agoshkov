f71ecc98097d02753ca9c72404e9f1d92c6bb6b8
Alexander Tsoy <Lils2010@gmail.com>
2017-10-17 18:04:54 +0300

    added q

--- src/main/java/ru/avtsoy/App.java
+++ src/main/java/ru/avtsoy/App.java
@@ -2,11 +2,8 @@ package ru.avtsoy;
 
 import java.util.Arrays;
 
-/**
- * Hello world!
- */
 public class App {
-    private static final double TAU = 1;
+    private static final double TAU = 0.5;
     private static final double A = 1;
     private static final double B = 1;
     private static final int N_X = 11;
@@ -16,30 +13,34 @@ public class App {
     private static final double MU = 1 / (2 * Math.PI);
     private static final double T = Math.PI / 2;
     private static final double ALPHA = 0.0001;
-    private static final int N_TIME_STEPS = 11;
+    private static final int N_TIME_STEPS = 10000;
     private static final double TIME_STEP = T / (N_TIME_STEPS - 1);
     private static final double[][][] f = new double[N_TIME_STEPS][N_X][N_Y];
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        double[][][] phi = new double[N_TIME_STEPS][N_X][N_Y];
-        phi = eqForPhi(phi);
-        System.out.println(Arrays.deepToString(phi[N_TIME_STEPS - 1]));
-        double[][][] q = new double[N_TIME_STEPS][N_X][N_Y];
-        q = eqForQ(q,phi);
-        System.out.println(Arrays.deepToString(q[0]));
+        double[][] u0 = generateU0();
+        double[][] u1 = generateU1();
+        int M = 5;
+        for (int l = 0; l < M; l++) {
+            double[][][] phi = new double[N_TIME_STEPS][N_X][N_Y];
+            phi = eqForPhi(phi,u0,u1);
+            double[][][] q = new double[N_TIME_STEPS][N_X][N_Y];
+            q = eqForQ(q, phi);
+            u0 = newU0(u0,q);
+            u1 = newU1(u1,q);
+        }
     }
 
-    private static double[][][] eqForPhi(double[][][] phi) {
+    private static double[][][] eqForPhi(double[][][] phi, double[][] u0, double[][] u1) {
         for (int j = 1; j < N_X - 1; j++) {
             for (int k = 1; k < N_Y - 1; k++) {
-                phi[0][j][k] = u0(j, k);
+                phi[0][j][k] = u0[j][k];
             }
         }
         phi[0] = corner(phi[0]);
         for (int j = 1; j < N_X - 1; j++) {
             for (int k = 1; k < N_Y - 1; k++) {
-                phi[1][j][k] = u1(j, k) * TIME_STEP + phi[0][j][k];
+                phi[1][j][k] = u1[j][k] * TIME_STEP + phi[0][j][k];
             }
         }
         phi[1] = corner(phi[1]);
@@ -84,12 +85,43 @@ public class App {
         return q;
     }
 
-    private static double u0(int j, int k) {
-        return 0;
+    private static double[][] generateU0() {
+        double[][] u0 = new double[N_X][N_Y];
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                u0[j][k] = 0;
+            }
+        }
+        return u0;
     }
 
-    private static double u1(int j, int k) {
-        return Math.sin(j * H_X) * Math.sin(k * H_Y);
+    private static double[][] generateU1() {
+        double[][] u1 = new double[N_X][N_Y];
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                u1[j][k] = 0;
+//                        Math.sin(j * H_X) * Math.sin(k * H_Y);
+            }
+        }
+        return u1;
+    }
+
+    private static double[][] newU0(double[][] u0, double[][][] q) {
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                u0[j][k] = u0[j][k] - TAU*(ALPHA*u0[j][k] - (q[1][j][k] - q[0][j][k])/TIME_STEP);
+            }
+        }
+        return u0;
+    }
+
+    private static double[][] newU1(double[][] u1, double[][][] q) {
+        for (int j = 1; j < N_X - 1; j++) {
+            for (int k = 1; k < N_Y - 1; k++) {
+                u1[j][k] = u1[j][k] - TAU*(ALPHA*u1[j][k] + q[0][j][k]);
+            }
+        }
+        return u1;
     }
 
     private static double varphi0(int j, int k) {
