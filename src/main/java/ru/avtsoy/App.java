63a8d1c9d978987cef80710b37d95c70472171a5
Alexander Tsoy <Lils2010@gmail.com>
2017-10-18 14:51:54 +0300

    added pictures and data

--- src/main/java/ru/avtsoy/App.java
+++ src/main/java/ru/avtsoy/App.java
@@ -17,74 +17,113 @@ public class App {
     private static final double H_Y = B / (N_Y - 1);
     private static final double MU = 1 / (2 * Math.pow(Math.PI, 2));
     private static final double T = Math.PI / 2;
-    private static double ALPHA = 0.001;
     private static final int N_TIME_STEPS = 110;
     private static final double TIME_STEP = T / (N_TIME_STEPS - 1);
     private static final double[][][] f = new double[N_TIME_STEPS][N_X][N_Y];
 
-    public static void main(String[] args) {
+    private static class Runner {
         double[][] u0 = generateU0();
         double[][] u1 = generateU1();
         double[][][] phi = new double[N_TIME_STEPS][N_X][N_Y];
         double[][][] q = new double[N_TIME_STEPS][N_X][N_Y];
+
+        Runner(int M, double alpha) {
+            for (int l = 0; l < M; l++) {
+                phi = eqForPhi(phi, u0, u1);
+                q = eqForQ(q, phi);
+                u0 = newU0(u0, q, alpha);
+                u1 = newU1(u1, q, alpha);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
         double[][] z = new double[N_X][N_Y];
-        double[][] phiT_t = new double[N_X][N_Y];
+        double[][] z0 = new double[N_X][N_Y];
         for (int j = 1; j < N_X - 1; j++) {
             for (int k = 1; k < N_Y - 1; k++) {
                 z[j][k] = Math.sin(Math.PI * j * H_X) * Math.sin(Math.PI * k * H_Y);
             }
         }
         double[] errors = new double[8];
-        double[] alphas = {0.1,0.01,0.001,0.0001,0.00001,0.000001,0.0000001,0.00000001};
-        for (int i = 0; i < 8; i++) {
-            ALPHA = alphas[i];
-            u0 = generateU0();
-            u1 = generateU1();
-            phi = new double[N_TIME_STEPS][N_X][N_Y];
-            q = new double[N_TIME_STEPS][N_X][N_Y];
-            phiT_t = new double[N_X][N_Y];
-            int M = 3;
-            for (int l = 0; l < M; l++) {
-                phi = eqForPhi(phi, u0, u1);
-                q = eqForQ(q, phi);
-                u0 = newU0(u0, q);
-                u1 = newU1(u1, q);
-            }
+        double[] alphas = {0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001};
+//        for (int i = 0; i < 8; i++) {
+//            double[][] phiT_t = new double[N_X][N_Y];
+//            Runner runner = new Runner(3,alphas[i]);
+//            for (int j = 1; j < N_X - 1; j++) {
+//                for (int k = 1; k < N_Y - 1; k++) {
+//                    phiT_t[j][k] = (runner.phi[N_TIME_STEPS - 1][j][k] - runner.phi[N_TIME_STEPS - 2][j][k]) / TIME_STEP;
+//                }
+//            }
+//            double varerror = 0;
+//            double weight = 0;
+//            for (int j = 1; j < N_X - 1; j++) {
+//                for (int k = 1; k < N_Y - 1; k++) {
+//                    varerror += Math.pow(runner.phi[N_TIME_STEPS - 1][j][k] - z[j][k], 2) * H_X * H_Y;
+//                    varerror += Math.pow(phiT_t[j][k] - 0, 2) * H_X * H_Y;
+//                    weight += Math.pow(z[j][k], 2) * H_X * H_Y;
+//                }
+//            }
+//            varerror = Math.sqrt(varerror / weight);
+//            errors[7-i] = varerror;
+//            System.out.println(varerror + ", " + alphas[i]);
+//        }
+//        for (int i = 0; i < 8; i++) {
+//            Runner runner = new Runner(3,alphas[i]);
+//            double varerror = 0;
+//            double weight = 0;
+//            for (int i1 = 0; i1 < N_TIME_STEPS; i1++) {
+//                for (int j = 1; j < N_X - 1; j++) {
+//                    for (int k = 1; k < N_Y - 1; k++) {
+//                        varerror += Math.pow(runner.phi[i1][j][k] -
+//                                Math.sin(Math.PI * j * H_X) * Math.sin(Math.PI * k * H_Y) *
+//                                        Math.sin((i1) * TIME_STEP), 2) * H_X * H_Y;
+//                        weight += Math.pow(Math.sin(Math.PI * j * H_X) * Math.sin(Math.PI * k * H_Y) *
+//                                Math.sin((i1) * TIME_STEP), 2) * H_X * H_Y;
+//                    }
+//                }
+//            }
+//            varerror = Math.sqrt(varerror / weight);
+//            errors[7 - i] = varerror;
+//            System.out.println(varerror + ", " + alphas[i]);
+//        }
 
-            for (int j = 1; j < N_X - 1; j++) {
-                for (int k = 1; k < N_Y - 1; k++) {
-                    phiT_t[j][k] = (phi[N_TIME_STEPS - 1][j][k] - phi[N_TIME_STEPS - 2][j][k]) / TIME_STEP;
-                }
-            }
-            double varerror = 0;
-            double weight = 0;
-            for (int j = 1; j < N_X - 1; j++) {
-                for (int k = 1; k < N_Y - 1; k++) {
-                    varerror += Math.pow(phi[N_TIME_STEPS - 1][j][k] - z[j][k], 2) * H_X * H_Y;
-                    varerror += Math.pow(phiT_t[j][k] - 0, 2) * H_X * H_Y;
-                    weight += Math.pow(z[j][k], 2) * H_X * H_Y;
-                }
-            }
-            varerror = Math.sqrt(varerror / weight);
-            errors[7-i] = varerror;
-            System.out.println(varerror + ", " + ALPHA);
-        }
-
+//        for (int i = 0; i < 8; i++) {
+//            Runner runner = new Runner(3,alphas[i]);
+//            double varerror = 0;
+//            double weight = 0;
+//                for (int j = 1; j < N_X - 1; j++) {
+//                    for (int k = 1; k < N_Y - 1; k++) {
+//                        varerror += Math.pow(runner.u0[j][k], 2) * H_X * H_Y;
+//                        varerror += Math.pow(runner.u1[j][k] -
+//                                Math.sin(Math.PI * j * H_X) * Math.sin(Math.PI * k * H_Y), 2) * H_X * H_Y;
+//                        weight += Math.pow(Math.sin(Math.PI * j * H_X) * Math.sin(Math.PI * k * H_Y), 2) * H_X * H_Y;
+//                    }
+//                }
+//
+//            varerror = Math.sqrt(varerror / weight);
+//            errors[7 - i] = varerror;
+//            System.out.println(varerror + ", " + alphas[i]);
+//        }
+        Runner runner = new Runner(3,0.001);
         double[] x = increment(0.0, H_X, 1.0); // x = 0.0:0.1:1.0
         double[] y = increment(0.0, H_Y, 1.0);// y = 0.0:0.05:1.0
-        double[] alpha_x = {-8,-7,-6,-5,-4,-3,-2,-1};
-        Plot2DPanel plot = new Plot2DPanel("SOUTH");
-//        Plot3DPanel plot = new Plot3DPanel("SOUTH");
-//        plot.addGridPlot("u0", x, y, u0);
-//        plot.addGridPlot("phiT", x, y, phi[N_TIME_STEPS-1]);
+        double[] alpha_x = {-8, -7, -6, -5, -4, -3, -2, -1};
+//        Plot2DPanel plot = new Plot2DPanel("SOUTH");
+        Plot3DPanel plot = new Plot3DPanel("SOUTH");
+        plot.addGridPlot("u0", x, y, runner.u0);
+        plot.addGridPlot("u0orig", x, y, z0);
+//        plot.addGridPlot("phiT", x, y, runner.phi[N_TIME_STEPS-1]);
 //        plot.addGridPlot("phiT_t", x, y, phiT_t);
 //        plot.addGridPlot("phi0", x, y, phi[0]);
-//        plot.addGridPlot("u1", x, y, u1);
+//        plot.addGridPlot("u1", x, y, runner.u1);
 //        plot.addGridPlot("u1orig", x, y, z);
 //        plot.addGridPlot("phiTorig", x, y, z);
-        plot.addLinePlot("J/J0",alpha_x,errors);
-        plot.setAxisLabel(0,"alpha");
-        plot.setAxisLabel(1,"");
+//        plot.addLinePlot("u/u0", alpha_x, errors);
+//        plot.setAxisLabel(0, "alpha");
+//        plot.setAxisLabel(1, "");
         JFrame frame = new JFrame("a plot panel");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
@@ -164,19 +203,19 @@ public class App {
         return u1;
     }
 
-    private static double[][] newU0(double[][] u0, double[][][] q) {
+    private static double[][] newU0(double[][] u0, double[][][] q, double alpha) {
         for (int j = 1; j < N_X - 1; j++) {
             for (int k = 1; k < N_Y - 1; k++) {
-                u0[j][k] = u0[j][k] - TAU * (ALPHA * u0[j][k] - (q[1][j][k] - q[0][j][k]) / TIME_STEP);
+                u0[j][k] = u0[j][k] - TAU * (alpha * u0[j][k] - (q[1][j][k] - q[0][j][k]) / TIME_STEP);
             }
         }
         return u0;
     }
 
-    private static double[][] newU1(double[][] u1, double[][][] q) {
+    private static double[][] newU1(double[][] u1, double[][][] q, double alpha) {
         for (int j = 1; j < N_X - 1; j++) {
             for (int k = 1; k < N_Y - 1; k++) {
-                u1[j][k] = u1[j][k] - TAU * (ALPHA * u1[j][k] + q[0][j][k]);
+                u1[j][k] = u1[j][k] - TAU * (alpha * u1[j][k] + q[0][j][k]);
             }
         }
         return u1;
